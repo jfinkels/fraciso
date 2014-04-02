468b4fe0d3554866380ca5023708983ee15b067c
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2014-04-02 16:51:05 -0400

    Bump version number to 0.0.4

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='nose.collector',
     tests_require=['nose'],
     url='https://github.com/jfinkels/fraciso',
-    version='0.0.4-dev',
+    version='0.0.4',
     zip_safe=False
 )
